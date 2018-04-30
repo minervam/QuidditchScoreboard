d10405951c898aee37e3ba774502e53f250805a1
Minerva Mendoza <minerva_mendoza@hotmail.com>
2018-04-29 19:48:21 -0700

    Removed unnecessary methods.

--- app/src/main/java/com/minervamendoza/quidditchscoreboard/MainActivity.java
+++ app/src/main/java/com/minervamendoza/quidditchscoreboard/MainActivity.java
@@ -1,16 +1,15 @@
 package com.minervamendoza.quidditchscoreboard;
 
-import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import static com.minervamendoza.quidditchscoreboard.R.menu.menu_main;
 
 /**
  * This activity keeps track of the basketball score for 2 teams.
@@ -33,78 +32,56 @@ public class MainActivity extends AppCompatActivity {
          */
 
         Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
-        // Create an ArrayAdapter using the string array and a default spinner layout
+        // ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
                 R.array.houses_array, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
+        // The layout to use when the list of choices appears
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
+        // Applies the adapter to the spinner
         spinner1.setAdapter(adapter1);
 
         /**
          * Dropdown menu for Team B
          */
         Spinner spinner2 = (Spinner) findViewById(R.id.spinner2);
-        // Create an ArrayAdapter using the string array and a default spinner layout
+        // ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
                 R.array.houses_array, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
+        // The layout to use when the list of choices appears
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
+        // Applies the adapter to the spinner
         spinner2.setAdapter(adapter2);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimpSlifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * Increase the score for Team A by 150 points.
-     */
-    public void addTwoForTeamA(View v) {
-        scoreTeamA = scoreTeamA + 150;
-        displayForTeamA(scoreTeamA);
     }
 
     /**
      * Increase the score for Team A by 10 points.
      */
-    public void addThreeForTeamA(View v) {
+    public void addTenForTeamA(View v) {
         scoreTeamA = scoreTeamA + 10;
         displayForTeamA(scoreTeamA);
     }
 
     /**
-     * Increase the score for Team B by 150 points.
+     * Increase the score for Team A by 150 points.
      */
-    public void addTwoForTeamB(View v) {
-        scoreTeamB = scoreTeamB + 150;
-        displayForTeamB(scoreTeamB);
+    public void addOneFiftyForTeamA(View v) {
+        scoreTeamA = scoreTeamA + 150;
+        displayForTeamA(scoreTeamA);
     }
 
     /**
      * Increase the score for Team B by 10 points.
      */
-    public void addThreeForTeamB(View v) {
+    public void addTenForTeamB(View v) {
         scoreTeamB = scoreTeamB + 10;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team B by 150 points.
+     */
+    public void addOneFiftyForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 150;
         displayForTeamB(scoreTeamB);
     }
 
